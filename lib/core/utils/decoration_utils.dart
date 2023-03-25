8c9f1046e97173970b6223136659ea079d11f9e3
aditgocendra <gocendra123@gmail.com>
2023-03-25 21:19:50 +0700

    add button style for dialog

--- lib/core/utils/decoration_utils.dart
+++ lib/core/utils/decoration_utils.dart
@@ -3,16 +3,15 @@ import 'package:flutter/material.dart';
 import '../constant/color_constant.dart';
 
 class DecorationUtils {
-  static InputDecoration textFieldDecoration(
-    String hint,
-  ) {
+  static InputDecoration textFieldDecoration({
+    required String hint,
+    required String label,
+  }) {
     return InputDecoration(
       fillColor: Colors.white60,
       hintText: hint,
-      contentPadding: const EdgeInsets.symmetric(
-        vertical: 16,
-        horizontal: 16,
-      ),
+      label: Text(label),
+      floatingLabelAlignment: FloatingLabelAlignment.start,
       border: const OutlineInputBorder(
         borderSide: BorderSide(
           color: borderColor,
@@ -21,6 +20,19 @@ class DecorationUtils {
           Radius.circular(12),
         ),
       ),
+    );
+  }
+
+  static ButtonStyle buttonDialogStyle() {
+    return ElevatedButton.styleFrom(
+      backgroundColor: primaryColor,
+      elevation: 0.5,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+      minimumSize: const Size.fromHeight(50),
     );
   }
 }
