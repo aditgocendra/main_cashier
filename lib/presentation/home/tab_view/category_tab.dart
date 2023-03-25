35c64f905307a6953ab54a0c7e147402a1d06a57
aditgocendra <gocendra123@gmail.com>
2023-03-25 21:20:35 +0700

    update

--- lib/presentation/home/tab_view/category_tab.dart
+++ lib/presentation/home/tab_view/category_tab.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:main_cashier/core/utils/decoration_utils.dart';
-import 'package:main_cashier/core/utils/dialog_utils.dart';
+import 'package:provider/provider.dart';
 import 'package:unicons/unicons.dart';
 
+import '../../../core/constant/values_constant.dart';
+import '../../../core/components/table_components.dart';
+import '../../../core/utils/decoration_utils.dart';
+import '../../../core/utils/dialog_utils.dart';
 import '../../../core/constant/color_constant.dart';
+import '../../../domain/entity/category_entity.dart';
 
-class CategoryTab extends StatelessWidget {
+import '../tab_controller/category_tab_controller.dart';
+
+class CategoryTab extends StatefulWidget {
   const CategoryTab({super.key});
 
   @override
+  State<CategoryTab> createState() => _CategoryTabState();
+}
+
+class _CategoryTabState extends State<CategoryTab> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<CategoryTabController>().setCategories();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final navigator = Navigator.of(context);
+    final ctgTabController = context.watch<CategoryTabController>();
+
     return Container(
       decoration: BoxDecoration(
         color: canvasColor,
@@ -29,21 +51,7 @@ class CategoryTab extends StatelessWidget {
                   child: TextField(
                     // controller: homeController.searchTec,
                     style: const TextStyle(fontSize: 14),
-                    onTap: () {
-                      // Change Content View
-                      // if (homeController.indexSidebarSelected.value < 2) {
-                      //   homeController.indexSidebarSelected.value = 1;
-                      //   return;
-                      // }
-
-                      // homeController.indexSidebarSelected.value =
-                      //     homeController.indexSidebarSelected.value;
-                    },
-                    onChanged: (value) {
-                      // if (value.isEmpty) {
-                      //   // homeController.setDataTable();
-                      // }
-                    },
+                    onChanged: (value) {},
                     decoration: InputDecoration(
                       fillColor: Colors.white60,
                       hintText: "Pencarian",
@@ -60,9 +68,7 @@ class CategoryTab extends StatelessWidget {
                         ),
                       ),
                       suffixIcon: InkWell(
-                        onTap: () {
-                          // homeController.search();
-                        },
+                        onTap: () {},
                         child: const Icon(
                           UniconsLine.search_alt,
                           color: borderColor,
@@ -86,7 +92,9 @@ class CategoryTab extends StatelessWidget {
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(16),
                               ),
-                              child: DialogCategoryAdd(),
+                              child: DialogCategoryAdd(
+                                ctgTabController: ctgTabController,
+                              ),
                             );
                           },
                         );
@@ -133,108 +141,100 @@ class CategoryTab extends StatelessWidget {
                     defaultVerticalAlignment: TableCellVerticalAlignment.middle,
                     children: <TableRow>[
                       // Header Data Table
-                      const TableRow(
-                        children: [
-                          TableCell(
-                            child: Padding(
-                              padding: EdgeInsets.all(16.0),
-                              child: Center(
-                                child: Text(
-                                  "Category ID",
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                          ),
-                          TableCell(
-                            child: Padding(
-                              padding: EdgeInsets.all(16.0),
-                              child: Center(
-                                child: Text(
-                                  "Title",
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                          ),
-                          TableCell(
-                            child: Padding(
-                              padding: EdgeInsets.all(16.0),
-                              child: Center(
-                                child: Text(
-                                  "Action",
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
+                      TableComponent.headerTable(
+                        ["Category ID", "Title", "Action"],
                       ),
-                      TableRow(
-                        children: [
-                          const TableCell(
-                            child: Padding(
-                              padding: EdgeInsets.all(16.0),
-                              child: Center(
-                                child: Text(
-                                  "Category ID",
-                                  style: TextStyle(fontSize: 14),
+                      // Body Data Table
+                      ...ctgTabController.listCategory.map((val) {
+                        return TableRow(
+                          children: [
+                            TableCell(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Center(
+                                  child: Text(
+                                    val.id.toString(),
+                                    style: const TextStyle(fontSize: 14),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          const TableCell(
-                            child: Padding(
-                              padding: EdgeInsets.all(16.0),
-                              child: Center(
-                                child: Text(
-                                  "Title",
-                                  style: TextStyle(fontSize: 14),
+                            TableCell(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Center(
+                                  child: Text(
+                                    val.title,
+                                    style: const TextStyle(fontSize: 14),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          TableCell(
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Center(
-                                child: Wrap(
-                                  children: [
-                                    // Edit
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(UniconsLine.edit),
-                                    ),
-                                    // Delete
-                                    IconButton(
-                                      onPressed: () {
-                                        showDialog(
-                                          context: context,
-                                          builder: (context) {
-                                            return DialogUtils
-                                                .dialogConfirmation(
-                                              title: "Delete Category",
-                                              message:
-                                                  "Are you sure delete this category ?",
-                                              callbackConfirmation: () {
-                                                Navigator.pop(context);
-                                              },
-                                              callbackCancel: () {
-                                                Navigator.pop(context);
-                                              },
-                                            );
-                                          },
-                                        );
-                                      },
-                                      icon: const Icon(UniconsLine.trash),
-                                    )
-                                  ],
+                            TableCell(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Center(
+                                  child: Wrap(
+                                    children: [
+                                      // Edit
+                                      IconButton(
+                                        onPressed: () {
+                                          showDialog(
+                                            context: context,
+                                            builder: (context) {
+                                              return Dialog(
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(16),
+                                                ),
+                                                child: DialogCategoryEdit(
+                                                  ctg: val,
+                                                  ctgTabController:
+                                                      ctgTabController,
+                                                ),
+                                              );
+                                            },
+                                          );
+                                        },
+                                        icon: const Icon(UniconsLine.edit),
+                                      ),
+                                      // Delete
+                                      IconButton(
+                                        onPressed: () {
+                                          showDialog(
+                                            context: context,
+                                            builder: (context) {
+                                              return DialogUtils
+                                                  .dialogConfirmation(
+                                                title: "Delete Category",
+                                                message:
+                                                    "Are you sure delete this category ?",
+                                                callbackConfirmation: () {
+                                                  ctgTabController
+                                                      .removeCategory(val);
+
+                                                  if (ctgTabController
+                                                      .errorDialog
+                                                      .isNotEmpty) {}
+                                                  navigator.pop();
+                                                },
+                                                callbackCancel: () {
+                                                  navigator.pop();
+                                                },
+                                              );
+                                            },
+                                          );
+                                        },
+                                        icon: const Icon(UniconsLine.trash),
+                                      )
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
+                          ],
+                        );
+                      }).toList()
                     ],
                   ),
                 ),
@@ -266,76 +266,143 @@ class CategoryTab extends StatelessWidget {
 }
 
 class DialogCategoryAdd extends StatelessWidget {
-  DialogCategoryAdd({super.key});
+  final TextEditingController tecTitle = TextEditingController();
+  final CategoryTabController ctgTabController;
 
-  final tecTitle = TextEditingController();
+  DialogCategoryAdd({required this.ctgTabController, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 800,
-      padding: const EdgeInsets.all(24),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                "Category Add",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              IconButton(
-                onPressed: () => Navigator.pop(context),
-                icon: const Icon(UniconsLine.times_circle),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          TextField(
-            controller: tecTitle,
-            style: const TextStyle(fontSize: 14),
-            onTap: () {
-              // Change Content View
-              // if (homeController.indexSidebarSelected.value < 2) {
-              //   homeController.indexSidebarSelected.value = 1;
-              //   return;
-              // }
+    final navigator = Navigator.of(context);
+    final formKey = GlobalKey<FormState>();
 
-              // homeController.indexSidebarSelected.value =
-              //     homeController.indexSidebarSelected.value;
+    return DialogUtils.layoutCustomDialog(
+      dialogHeaderText: "Category Add",
+      childern: [
+        Form(
+          key: formKey,
+          child: TextFormField(
+            controller: tecTitle,
+            validator: (title) {
+              if (title == null || title.isEmpty) {
+                return fieldRequired;
+              }
+
+              return null;
             },
-            onChanged: (value) {
-              // if (value.isEmpty) {
-              //   // homeController.setDataTable();
-              // }
-            },
-            decoration: DecorationUtils.textFieldDecoration("Title"),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: primaryColor,
-              elevation: 0.5,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-              ),
-              minimumSize: const Size.fromHeight(50),
+            style: const TextStyle(fontSize: 14),
+            decoration: DecorationUtils.textFieldDecoration(
+              label: "Title",
+              hint: "Example : Accesoris",
             ),
-            child: const Text("Add"),
-          )
-        ],
-      ),
+          ),
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (!formKey.currentState!.validate()) {
+              return;
+            }
+
+            ctgTabController.addCategory(tecTitle.text);
+            navigator.pop();
+
+            if (ctgTabController.errorDialog.isNotEmpty) {
+              showDialog(
+                context: context,
+                builder: (context) => DialogUtils.dialogInformation(
+                  title: "Edit Category",
+                  message: ctgTabController.errorDialog,
+                  callbackConfirmation: () => navigator.pop(),
+                ),
+              );
+              ctgTabController.resetErrorDialog();
+            }
+          },
+          style: DecorationUtils.buttonDialogStyle(),
+          child: const Text("Add"),
+        ),
+      ],
+      callbackClose: () {
+        navigator.pop();
+      },
+    );
+  }
+}
+
+class DialogCategoryEdit extends StatelessWidget {
+  final TextEditingController tecTitle = TextEditingController();
+
+  final CategoryTabController ctgTabController;
+  final CategoryEntity ctg;
+
+  DialogCategoryEdit({
+    required this.ctgTabController,
+    required this.ctg,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final navigator = Navigator.of(context);
+    final formKey = GlobalKey<FormState>();
+
+    tecTitle.text = ctg.title;
+
+    return DialogUtils.layoutCustomDialog(
+      dialogHeaderText: "Category Edit",
+      childern: [
+        Form(
+          key: formKey,
+          child: TextFormField(
+            controller: tecTitle,
+            validator: (title) {
+              if (title == null || title.isEmpty) {
+                return fieldRequired;
+              }
+              return null;
+            },
+            style: const TextStyle(fontSize: 14),
+            decoration: DecorationUtils.textFieldDecoration(
+              label: "Title",
+              hint: "Example : Accesoris",
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (!formKey.currentState!.validate()) {
+              return;
+            }
+
+            ctg.title = tecTitle.text;
+            ctgTabController.updateCategory(ctg);
+
+            navigator.pop();
+
+            if (ctgTabController.errorDialog.isNotEmpty) {
+              showDialog(
+                context: context,
+                builder: (context) => DialogUtils.dialogInformation(
+                  title: "Edit Category",
+                  message: ctgTabController.errorDialog,
+                  callbackConfirmation: () => navigator.pop(),
+                ),
+              );
+
+              ctgTabController.resetErrorDialog();
+            }
+          },
+          style: DecorationUtils.buttonDialogStyle(),
+          child: const Text("Edit"),
+        ),
+      ],
+      callbackClose: () => navigator.pop(),
     );
   }
 }
