60268181792a4694b8b98dee107f404272c304c3
aditgocendra <gocendra123@gmail.com>
2023-03-25 21:19:25 +0700

    add

--- lib/core/components/table_components.dart
+++ lib/core/components/table_components.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+class TableComponent {
+  static TableCell _headerComponent(String text) {
+    return TableCell(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ),
+      ),
+    );
+  }
+
+  static TableRow headerTable(List<String> headerText) {
+    return TableRow(
+      children: headerText.map((val) {
+        return _headerComponent(val);
+      }).toList(),
+    );
+  }
+}
