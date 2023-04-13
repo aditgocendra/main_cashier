0ffab1c938cb716cca97eb7048831eb8390985a6
aditgocendra <gocendra123@gmail.com>
2023-04-13 14:02:26 +0700

    remove create, update, delete

--- lib/data/repositories/role_repository_impl.dart
+++ lib/data/repositories/role_repository_impl.dart
@@ -1,7 +1,6 @@
-import 'package:main_cashier/data/datasource/local/role_local_datasource.dart';
-import 'package:main_cashier/data/models/role_model.dart';
-import 'package:main_cashier/domain/entity/role_entity.dart';
-import 'package:main_cashier/domain/repostitories/role_repository.dart';
+import '../datasource/local/role_local_datasource.dart';
+import '../../domain/entity/role_entity.dart';
+import '../../domain/repostitories/role_repository.dart';
 
 import '../../core/error/exception.dart';
 
@@ -13,43 +12,11 @@ class RoleRepositoryImpl implements RoleRepository {
   });
 
   @override
-  Future<RoleEntity> createRole(String name) async {
-    try {
-      return await roleLocalDataSource.create(name);
-    } catch (_) {
-      throw DatabaseDriftException("Insert data role fail");
-    }
-  }
-
-  @override
   Future<List<RoleEntity>> getRole() async {
     try {
       return await roleLocalDataSource.getAll();
     } catch (_) {
       throw DatabaseDriftException("Fetch data role fail");
-    }
-  }
-
-  @override
-  Future<int> deleteRole(int id) async {
-    try {
-      return await roleLocalDataSource.delete(id);
-    } catch (_) {
-      throw DatabaseDriftException("Delete data role fail");
-    }
-  }
-
-  @override
-  Future<bool> updateRole(RoleEntity roleEntity) async {
-    try {
-      return await roleLocalDataSource.update(
-        RoleModel(
-          id: roleEntity.id,
-          name: roleEntity.name,
-        ),
-      );
-    } catch (e) {
-      throw DatabaseDriftException("Update data role fail");
     }
   }
 }
--- lib/domain/repostitories/role_repository.dart
+++ lib/domain/repostitories/role_repository.dart
@@ -1,11 +1,5 @@
-import 'package:main_cashier/domain/entity/role_entity.dart';
+import '../entity/role_entity.dart';
 
 abstract class RoleRepository {
-  Future<RoleEntity> createRole(String name);
-
   Future<List<RoleEntity>> getRole();
-
-  Future<bool> updateRole(RoleEntity roleEntity);
-
-  Future<int> deleteRole(int id);
 }
