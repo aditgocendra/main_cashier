d22f8482ef12039b5b0c46b937723fe05443888d
aditgocendra <gocendra123@gmail.com>
2023-04-24 14:20:56 +0700

    refactory

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,31 +1,10 @@
-import 'package:main_cashier/data/datasource/local/transaction_local_datasource.dart';
-import 'package:main_cashier/data/datasource/local/user_local_datasource.dart';
-import 'package:main_cashier/data/repositories/transaction_repository_impl.dart';
-import 'package:main_cashier/data/repositories/user_repository_impl.dart';
-import 'package:main_cashier/domain/repostitories/transaction_repository.dart';
-import 'package:main_cashier/domain/repostitories/user_repository.dart';
-import 'package:main_cashier/domain/usecase/product/select_product_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/create_transaction_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/delete_transaction_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/get_all_transaction_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/get_counter_transaction_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/get_detail_transaction_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/get_omzet_transaction_with_range_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/get_profit_transaction_with_range_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/get_report_transactions_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/get_transaction_with_range_date_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/search_transaction_usecase.dart';
-import 'package:main_cashier/domain/usecase/transaction/update_counter_transaction_usecase.dart';
-import 'package:main_cashier/domain/usecase/user/change_pass_user_usecase.dart';
-import 'package:main_cashier/domain/usecase/user/create_user_usecase.dart';
-import 'package:main_cashier/domain/usecase/user/delete_user_usecase.dart';
-import 'package:main_cashier/domain/usecase/user/get_view_user_usecase.dart';
-import 'package:main_cashier/domain/usecase/user/update_user_usecase.dart';
-import 'package:main_cashier/presentation/home/tab_controller/transaction_tab_controller.dart';
-import 'package:main_cashier/presentation/transaction/transaction_controller.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
+import 'data/datasource/local/transaction_local_datasource.dart';
+import 'data/datasource/local/user_local_datasource.dart';
+import 'data/repositories/transaction_repository_impl.dart';
+import 'data/repositories/user_repository_impl.dart';
 import 'data/datasource/local/product_local_datasource.dart';
 import 'data/repositories/product_repository_impl.dart';
 import 'data/datasource/local/category_local_datasource.dart';
@@ -47,7 +26,25 @@ import 'domain/repostitories/category_repository.dart';
 import 'domain/usecase/category/create_category_usecase.dart';
 import 'domain/usecase/category/get_categories_usecase.dart';
 import 'domain/repostitories/role_repository.dart';
-
+import 'domain/repostitories/transaction_repository.dart';
+import 'domain/repostitories/user_repository.dart';
+import 'domain/usecase/product/select_product_usecase.dart';
+import 'domain/usecase/transaction/create_transaction_usecase.dart';
+import 'domain/usecase/transaction/delete_transaction_usecase.dart';
+import 'domain/usecase/transaction/get_all_transaction_usecase.dart';
+import 'domain/usecase/transaction/get_counter_transaction_usecase.dart';
+import 'domain/usecase/transaction/get_detail_transaction_usecase.dart';
+import 'domain/usecase/transaction/get_omzet_transaction_with_range_usecase.dart';
+import 'domain/usecase/transaction/get_profit_transaction_with_range_usecase.dart';
+import 'domain/usecase/transaction/get_report_transactions_usecase.dart';
+import 'domain/usecase/transaction/search_transaction_usecase.dart';
+import 'domain/usecase/transaction/update_counter_transaction_usecase.dart';
+import 'domain/usecase/user/change_pass_user_usecase.dart';
+import 'domain/usecase/user/create_user_usecase.dart';
+import 'domain/usecase/user/delete_user_usecase.dart';
+import 'domain/usecase/user/get_view_user_usecase.dart';
+import 'domain/usecase/user/search_user_usecase.dart';
+import 'domain/usecase/user/update_user_usecase.dart';
 import 'domain/usecase/role/get_role_usecase.dart';
 
 import 'presentation/home/home_controller.dart';
@@ -55,6 +52,8 @@ import 'presentation/home/tab_controller/category_tab_controller.dart';
 import 'presentation/home/tab_controller/inventory_tab_controller.dart';
 import 'presentation/sign_in/sign_in_controller.dart';
 import 'presentation/home/tab_controller/users_tab_controller.dart';
+import 'presentation/home/tab_controller/transaction_tab_controller.dart';
+import 'presentation/transaction/transaction_controller.dart';
 
 List<SingleChildWidget> get listProvider => _listProvider;
 
@@ -176,6 +175,10 @@ ChangePassword _changePassword = ChangePassword(
   repository: _userRepository,
 );
 
+SearchUser _searchUser = SearchUser(
+  repository: _userRepository,
+);
+
 // Transaction Usecase
 CreateTransaction _createTransaction = CreateTransaction(
   repository: _transactionRepository,
@@ -186,11 +189,6 @@ GetAllTransaction _getAllTransaction = GetAllTransaction(
 );
 
 GetDetailTransaction _getDetailTransaction = GetDetailTransaction(
-  repository: _transactionRepository,
-);
-
-GetTransactionWithRangeDate _getTransactionWithRangeDate =
-    GetTransactionWithRangeDate(
   repository: _transactionRepository,
 );
 
@@ -256,6 +254,7 @@ List<SingleChildWidget> _listProvider = [
       updateUser: _updateUser,
       deleteUser: _deleteUser,
       changePassword: _changePassword,
+      searchUser: _searchUser,
     ),
   ),
   ChangeNotifierProvider(
