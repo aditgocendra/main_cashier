994d561b8d2870263c39f26eced4cef5aafa2ebb
aditgocendra <gocendra123@gmail.com>
2023-05-13 11:49:10 +0700

    sign in

--- lib/presentation/sign_in/sign_in_controller.dart
+++ lib/presentation/sign_in/sign_in_controller.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:main_cashier/domain/usecase/login_info/create_login_info_usecase.dart';
+import '../../domain/usecase/user/get_user_with_username_usecase.dart';
 
-class SignInController extends ChangeNotifier {}
+class SignInController extends ChangeNotifier {
+  final GetUserWithUsername getUserWithUsername;
+  final CreateLoginInfo createLoginInfo;
+
+  SignInController({
+    required this.getUserWithUsername,
+    required this.createLoginInfo,
+  });
+
+  void signIn({
+    required String username,
+    required String pass,
+    required VoidCallback callbackSuccess,
+    required VoidCallback callbackFail,
+  }) async {
+    await getUserWithUsername.call(username).then((value) async {
+      if (value.password != pass) {
+        return;
+      }
+
+      await createLoginInfo.call(value.uid).then(
+            (value) => callbackSuccess.call(),
+          );
+    });
+  }
+
+  void setLoginInfo(String uid) async {}
+}
