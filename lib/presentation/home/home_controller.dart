5c23565057fa41feb007a89e42ba98a140f362fa
aditgocendra <gocendra123@gmail.com>
2023-05-13 11:50:01 +0700

    update

--- lib/presentation/home/home_controller.dart
+++ lib/presentation/home/home_controller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:main_cashier/domain/usecase/login_info/delete_login_info_usecase.dart';
+import '../../core/usecase/usecase.dart';
+import '../../domain/usecase/user/get_total_user_usecase.dart';
 
 class HomeController extends ChangeNotifier {
   late TabController tabControllerHome;
@@ -9,14 +12,39 @@ class HomeController extends ChangeNotifier {
   bool _isSidebarExpanded = false;
   bool get isSidebarExpanded => _isSidebarExpanded;
 
-  void toogleSidebar(bool expand) {
-    _isSidebarExpanded = expand;
-    // notifyListeners();
+  final GetTotalUser getTotalUser;
+  final DeleteLoginInfo deleteLoginInfo;
+
+  HomeController({
+    required this.getTotalUser,
+    required this.deleteLoginInfo,
+  });
+
+  Future checkUsers({
+    required VoidCallback callbackUserNotExist,
+  }) async {
+    await getTotalUser.call(NoParans()).then((value) {
+      if (value == null || value == 0) {
+        callbackUserNotExist.call();
+      }
+    });
   }
+
+  void toogleSidebar(bool expand) => _isSidebarExpanded = expand;
 
   void changeIndexTab(int index) {
     _indexTabActive = index;
     tabControllerHome.index = index;
     notifyListeners();
+  }
+
+  void signOut(VoidCallback callback) async {
+    await deleteLoginInfo.call('asd').then((value) {
+      callback.call();
+    });
+  }
+
+  void test(VoidCallback callback) {
+    callback.call();
   }
 }
